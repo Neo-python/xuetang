2601060034a39d0c597b1ce4be83ee1b0503f0c2
Neo-python <g602049338@icloud.com>
2018-07-12 23:06:41 +0800

    Update settings.py

--- xuetang/settings.py
+++ xuetang/settings.py
@@ -133,4 +133,4 @@ TINYMCE_JS_URL = "/static/js/tinymce/timymce.min.js"
 
 DOMAIN_NAME = 'http://127.0.0.1:8000'
 
-SYMBOL_NUMBER = {58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96}
+SYMBOL_NUMBER = {58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96}  # 符号ascii表,a-Z之间的符号代码
